ef766ea890b42803cc1c438c3820c1296ba649b9
HeartlandNetwork <gareth_rowell@nps.gov>
2024-09-03 17:18:05 -0500

    first code

---
+++ PartOne/src/observer_compare1.py
@@ -0,0 +1,23 @@
+
+
+
+###############################################################
+#
+#   observer_compare1.py
+#   Purpose is to load and do initial comparisons
+#   between observer data from MS Access and SQL Server
+#
+#   Gareth Rowell - 20240903
+#
+###############################################################
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+
+df = pd.read_csv("periods_events.csv")
+
